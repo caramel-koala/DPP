fbf25156c270e1fdbdc955628393c224a1e846dd
caramel-koala <antoniob.peters@gmail.com>
2016-08-21 23:18:56 +0200

    Think prac 3 is done?

--- Prac 3/MessageStorage/Execute.sh
+++ Prac 3/MessageStorage/Execute.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 rmiregistry 2001 &
+
+javac MessageStorage.java
+javac MessageServer.java
+javac MessageClient.java
+
+java MessageServer
+java MessageClient
--- Prac 3/MessageStorage/MessageClient.java
+++ Prac 3/MessageStorage/MessageClient.java
@@ -1,6 +1,7 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class MessageClient
 {
@@ -53,10 +54,28 @@ public class MessageClient
   public static void main (String[] args)
   {
     Scanner reader = new Scanner(System.in);  // Reading from System.in
-    while (True)
+    MessageClient user = new MessageClient();
+    while (true)
     {
       System.out.println("(1=NewMessage,2=Readmessage,0=Exit): ");
       int n = reader.nextInt();
+      reader.nextLine();
+      switch (n)
+      {
+        case 0: System.exit(0);
+        case 1: System.out.println("Enter Message: ");
+                int code = user.Write(reader.nextLine());
+                System.out.println("Message #" + code + " recorded.");
+                reader.nextLine();
+                break;
+        case 2: System.out.println("Enter Message Code: ");
+                int num = reader.nextInt();
+                String msg = user.Read(num);
+                reader.nextLine();
+                System.out.println("Message #" + num + ": " + msg);
+                reader.nextLine();
+                break;
+      }
 
     }
   }
